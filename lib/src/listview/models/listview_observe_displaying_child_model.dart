b12c8254db968d810cecdf52bc13f6316df75d74
LinXunFeng <linxunfeng@yeah.net>
2022-06-21 23:43:43 +0800

    feat (SliverList): Provide the display percentage of the current widget

--- lib/src/listview/models/listview_observe_displaying_child_model.dart
+++ lib/src/listview/models/listview_observe_displaying_child_model.dart
@@ -10,8 +10,20 @@ class ListViewObserveDisplayingChildModel {
   /// The renderObject [RenderBox] of child widget.
   final RenderBox renderObject;
 
-  /// Size of child
+  /// The axis of sliverList
+  Axis get axis => sliverList.constraints.axis;
+
+  /// The size of child
   Size get size => renderObject.size;
+
+  /// The size of child on the main axis
+  double get mainAxisSize => axis == Axis.vertical ? size.height : size.width;
+
+  /// The scroll offset of sliverList
+  double get scrollOffset => sliverList.constraints.scrollOffset;
+
+  /// The display percentage of the current widget
+  double get displayPercentage => calculateDisplayPercentage();
 
   ListViewObserveDisplayingChildModel({
     required this.sliverList,
@@ -32,5 +44,27 @@ class ListViewObserveDisplayingChildModel {
   @override
   int get hashCode {
     return index + renderObject.hashCode;
+  }
+
+  /// Calculates the display percentage of the current widget
+  double calculateDisplayPercentage() {
+    final parentData = renderObject.parentData;
+    if (parentData is! SliverMultiBoxAdaptorParentData) return 0;
+
+    final currentChildLayoutOffset = parentData.layoutOffset ?? 0;
+    double remainingMainAxisSize = mainAxisSize;
+    if (scrollOffset > currentChildLayoutOffset) {
+      remainingMainAxisSize =
+          mainAxisSize - (scrollOffset - currentChildLayoutOffset);
+    } else {
+      final childWidgetMaxY = mainAxisSize + currentChildLayoutOffset;
+      final listContentMaxY =
+          scrollOffset + sliverList.constraints.remainingPaintExtent;
+      if (childWidgetMaxY > listContentMaxY) {
+        remainingMainAxisSize =
+            mainAxisSize - (childWidgetMaxY - listContentMaxY);
+      }
+    }
+    return (remainingMainAxisSize / mainAxisSize).clamp(0, 1);
   }
 }
