11fa282ab21a523d64bf8c0c5e32708056764249
LinXunFeng <linxunfeng@yeah.net>
2023-08-17 23:20:17 +0800

    fix: compatible with flutter 3.13.0

--- lib/src/utils/src/observer_utils.dart
+++ lib/src/utils/src/observer_utils.dart
@@ -192,7 +192,14 @@ class ObserverUtils {
   static RenderViewportBase? findViewport(RenderObject obj) {
     int maxCycleCount = 10;
     int currentCycleCount = 1;
-    AbstractNode? parent = obj.parent;
+    // Starting from flutter version 3.13.0, the type of parent received 
+    // is RenderObject, while the type of the previous version is AbstractNode,
+    // but RenderObject is a subclass of AbstractNode, so for compatibility,
+    // we can use RenderObject.
+    var parent = obj.parent;
+    if (parent is! RenderObject) {
+      return null;
+    }
     while (parent != null && currentCycleCount <= maxCycleCount) {
       if (parent is RenderViewportBase) {
         return parent;
