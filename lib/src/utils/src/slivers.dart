f15ff9c4d0fcd7b3a8841dcfc0e160cd12718426
LinXunFeng <linxunfeng@yeah.net>
2024-05-19 10:30:40 +0800

    feat(sliver): add SliverObserveContext

--- lib/src/utils/src/slivers.dart
+++ lib/src/utils/src/slivers.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
+class SliverObserveContext extends SliverPadding {
+  final void Function(BuildContext) onObserve;
+  const SliverObserveContext({
+    Key? key,
+    Widget? child,
+    required this.onObserve,
+  }) : super(
+          key: key,
+          padding: EdgeInsets.zero,
+          sliver: child,
+        );
+
+  @override
+  RenderSliverPadding createRenderObject(BuildContext context) {
+    onObserve.call(context);
+    return super.createRenderObject(context);
+  }
+}
+
 class SliverObserveContextToBoxAdapter extends SliverToBoxAdapter {
   final void Function(BuildContext) onObserve;
 
