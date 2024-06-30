76d81b859d659295c845d7ee0af9da0c80d2d360
LinXunFeng <linxunfeng@yeah.net>
2024-06-30 17:52:10 +0800

    feat(NestedScrollUtil): add jumpTo and animateTo methods

--- lib/src/utils/src/nested_scroll_util.dart
+++ lib/src/utils/src/nested_scroll_util.dart
@@ -7,6 +7,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:scrollview_observer/scrollview_observer.dart';
+import 'package:scrollview_observer/src/common/typedefs.dart';
+
+enum NestedScrollUtilPosition {
+  /// Corresponds to the headerSliver in [NestedScrollView].
+  header,
+
+  /// Corresponds to the body in [NestedScrollView].
+  body,
+}
 
 class NestedScrollUtil {
   /// Record the [BuildContext] corresponding to all header slivers of
@@ -22,6 +31,12 @@ class NestedScrollUtil {
 
   /// Record the [RenderObject] of [SliverFillRemaining].
   RenderSliverSingleBoxAdapter? remainingSliverRenderObj;
+
+  /// The outer [ScrollController] of a NestedScrollView.
+  ScrollController? outerScrollController;
+
+  /// The inner [ScrollController] in the body of a NestedScrollView.
+  ScrollController? bodyScrollController;
 
   /// Calculate the overlap for the body sliver.
   double? calcOverlap({
@@ -62,6 +77,7 @@ class NestedScrollUtil {
     return sliverContextExtraOverlap + _obj.constraints.overlap;
   }
 
+  /// Calculate the [precedingScrollExtent] for [sliverContext].
   double? calcPrecedingScrollExtent({
     required GlobalKey nestedScrollViewKey,
     required BuildContext sliverContext,
@@ -89,6 +105,8 @@ class NestedScrollUtil {
     bodySliverContexts.clear();
     remainingSliverContext = null;
     remainingSliverRenderObj = null;
+    outerScrollController = null;
+    bodyScrollController = null;
   }
 
   /// Get SliverFillRemaining
@@ -110,5 +128,158 @@ class NestedScrollUtil {
       },
     );
     return remainingSliverContext;
+  }
+
+  /// Jump to the specified index position.
+  jumpTo({
+    required GlobalKey nestedScrollViewKey,
+    required SliverObserverController observerController,
+    required NestedScrollUtilPosition position,
+    required int index,
+    required BuildContext? sliverContext,
+    bool isFixedHeight = false,
+    double alignment = 0,
+    EdgeInsets padding = EdgeInsets.zero,
+    ObserverLocateIndexOffsetCallback? offset,
+    ObserverRenderSliverType? renderSliverType,
+  }) {
+    assert(outerScrollController != null, 'outerScrollController is null');
+    assert(bodyScrollController != null, 'bodyScrollController is null');
+    if (outerScrollController == null) return;
+    if (bodyScrollController == null) return;
+    switchScrollController(
+      observerController: observerController,
+      position: position,
+    );
+
+    observerController.jumpTo(
+      index: index,
+      sliverContext: sliverContext,
+      isFixedHeight: isFixedHeight,
+      alignment: alignment,
+      padding: padding,
+      offset: offset,
+      renderSliverType: renderSliverType,
+      onPrepareScrollToIndex: handleOnPrepareScrollToIndex(
+        nestedScrollViewKey: nestedScrollViewKey,
+        position: position,
+        outerScrollController: outerScrollController!,
+        offset: offset,
+      ),
+    );
+  }
+
+  /// Animate to the specified index position.
+  animateTo({
+    required GlobalKey nestedScrollViewKey,
+    required SliverObserverController observerController,
+    required NestedScrollUtilPosition position,
+    required int index,
+    required Duration duration,
+    required Curve curve,
+    required BuildContext? sliverContext,
+    bool isFixedHeight = false,
+    double alignment = 0,
+    EdgeInsets padding = EdgeInsets.zero,
+    ObserverLocateIndexOffsetCallback? offset,
+    ObserverRenderSliverType? renderSliverType,
+  }) {
+    assert(outerScrollController != null, 'outerScrollController is null');
+    assert(bodyScrollController != null, 'bodyScrollController is null');
+    if (outerScrollController == null) return;
+    if (bodyScrollController == null) return;
+    switchScrollController(
+      observerController: observerController,
+      position: position,
+    );
+
+    observerController.animateTo(
+      index: index,
+      duration: duration,
+      curve: curve,
+      sliverContext: sliverContext,
+      isFixedHeight: isFixedHeight,
+      alignment: alignment,
+      padding: padding,
+      offset: offset,
+      renderSliverType: renderSliverType,
+      onPrepareScrollToIndex: handleOnPrepareScrollToIndex(
+        nestedScrollViewKey: nestedScrollViewKey,
+        position: position,
+        outerScrollController: outerScrollController!,
+        duration: duration,
+        curve: curve,
+        offset: offset,
+      ),
+    );
+  }
+
+  /// Switch the [ScrollController] of [observerController] according to the
+  /// [NestedScrollUtilPosition].
+  switchScrollController({
+    required SliverObserverController observerController,
+    required NestedScrollUtilPosition position,
+  }) {
+    assert(outerScrollController != null, 'outerScrollController is null');
+    assert(bodyScrollController != null, 'bodyScrollController is null');
+    if (outerScrollController == null) return;
+    if (bodyScrollController == null) return;
+
+    switch (position) {
+      case NestedScrollUtilPosition.header:
+        observerController.controller = outerScrollController;
+        break;
+      case NestedScrollUtilPosition.body:
+        observerController.controller = bodyScrollController;
+        break;
+    }
+  }
+
+  /// Handle the [onPrepareScrollToIndex] callback.
+  ObserverOnPrepareScrollToIndex? handleOnPrepareScrollToIndex({
+    required GlobalKey nestedScrollViewKey,
+    required NestedScrollUtilPosition position,
+    required ScrollController outerScrollController,
+    Duration? duration,
+    Curve? curve,
+    ObserverLocateIndexOffsetCallback? offset,
+  }) {
+    switch (position) {
+      case NestedScrollUtilPosition.header:
+        return null;
+      case NestedScrollUtilPosition.body:
+        return (calcResult) async {
+          if (calcResult.calculateTargetLayoutOffset > 0) {
+            // Here we can get the item's offset accurately.
+            return false;
+          }
+          // The item is located relatively top, and the accurate item
+          // offset cannot be obtained.
+          // So here we jump through outerScrollController.
+          final remainingSliverContext = fetchRemainingSliverContext(
+            nestedScrollViewKey: nestedScrollViewKey,
+          );
+          var remainingSliverContextObj = ObserverUtils.findRenderObject(
+            remainingSliverContext,
+          );
+          if (remainingSliverContextObj is! RenderSliverSingleBoxAdapter) {
+            return false;
+          }
+          double targetOffset =
+              remainingSliverContextObj.constraints.precedingScrollExtent;
+          targetOffset -= offset?.call(targetOffset) ?? 0;
+          targetOffset += calcResult.targetChildLayoutOffset;
+          if (duration != null && curve != null) {
+            outerScrollController.animateTo(
+              targetOffset,
+              duration: duration,
+              curve: curve,
+            );
+          } else {
+            outerScrollController.jumpTo(targetOffset);
+          }
+          return true;
+        };
+    }
   }
 }
